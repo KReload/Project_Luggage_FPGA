9a3f99f29648a1290f4534e0feabdb053c46e521
BESSIERES Fabien <3407573@pc4072.polytech.upmc.fr>
2018-12-06 15:20:00 +0100

    mem.vhd

--- VHDL/FD/framediff.vhd
+++ VHDL/FD/framediff.vhd
@@ -4,15 +4,22 @@ use ieee.numeric_std.all;
 
 entity framediff is
   port(
-    I0, I1 : std_logic_vector(7 downto 0);
+    clk,rst : in std_logic;
     Et : out std_logic_vector(7 downto 0)
     );
 end entity;
 
 architecture framediff_arch of framediff is
-  signal sabsOut, theta : std_logic_vector(7 downto 0);
+  constant N : integer := 2;
+  signal sabsOut, theta, I0, I1 : std_logic_vector(7 downto 0);
+  signal addr0outMAE : unsigned(17 downto 0) := "000000000000000000";
+  signal addr1outMAE : unsigned(17 downto 0) := "010100101000000000";
+  signal irq0, irq1 : std_logic;
 begin
   theta <= "00000110"; --theta = 6
+  
+  MAE: entity work.MAE(MAE_arch) port map (clk => clk, rst => rst, addr0_prec => addr0outMAE, addr1_prec => addr1outMAE, addr0 => addr0outMAE, addr1 => addr1outMAE, irq_mem0 => irq0, irq_mem1 => irq1);
+  MEMORY : entity work.mem(mem_arch) generic map (N => N) port map (addr0 => addr0outMAE, addr1 => addr1outMAE, q0 => I0, q1 => I1);
   SABS: entity work.sabs(sabs_arch) port map (A => I1, B => I0,S => sabsOut);
   CMP: entity work.cmp(cmp_arch) port map (A => sabsOut, B => theta, S => Et);
 end architecture;
--- VHDL/testbench/mem_tb.vhd
+++ VHDL/testbench/mem_tb.vhd
@@ -8,17 +8,21 @@ end mem_tb ;
 
 
 architecture BENCH of mem_tb is
-  
+  constant N : integer := 2;
+  signal addr0, addr1 : unsigned(17 downto 0);
+  signal q0,q1 : std_logic_vector(7 downto 0);
 begin
-  UUT : entity work.mem port map ();
-    
+  UUT : entity work.mem
+    generic map(N => N)
+    port map (addr0 => addr0,addr1 => addr1,q0 => q0,q1 => q1);
+  
   process
   begin
     addr0 <= "000000000000000000";
     addr1 <= "000000000000000001";
     wait for 2 ns;
-    assert q0 = "000000000000000001" report "Error on addr0 = 1" severity warning;
-    assert q1 = "000000000000000010" report "Error on addr1 = 2" severity warning;
+    assert q0 = "00000000" report "Error on q0 = 0" severity warning;
+    assert q1 = "11111111" report "Error on q1 = 255" severity warning;
     wait for 10 ns;
 
     report "End of test. Verify that no error was reported.";
